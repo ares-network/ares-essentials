80246e616f460c0e891067aadfb0351dc7cf0369
j-rockwell <jrobertrockwell@gmail.com>
2020-04-01 20:21:21 -0700

    Added broadcast command

--- src/main/java/com/llewkcor/ares/essentials/Essentials.java
+++ src/main/java/com/llewkcor/ares/essentials/Essentials.java
@@ -1,6 +1,7 @@
 package com.llewkcor.ares.essentials;
 
 import co.aikar.commands.PaperCommandManager;
+import com.llewkcor.ares.essentials.command.ChatCommand;
 import com.llewkcor.ares.essentials.command.ItemCommand;
 import com.llewkcor.ares.essentials.command.PlayerCommand;
 import lombok.Getter;
@@ -21,6 +22,7 @@ public final class Essentials extends JavaPlugin {
         commandManager.enableUnstableAPI("help");
         commandManager.registerCommand(new ItemCommand(this));
         commandManager.registerCommand(new PlayerCommand(this));
+        commandManager.registerCommand(new ChatCommand(this));
     }
 
     @Override
---
+++ src/main/java/com/llewkcor/ares/essentials/command/ChatCommand.java
@@ -0,0 +1,38 @@
+package com.llewkcor.ares.essentials.command;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Description;
+import co.aikar.commands.annotation.Syntax;
+import com.llewkcor.ares.essentials.Essentials;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+@AllArgsConstructor
+public final class ChatCommand extends BaseCommand {
+    @Getter public final Essentials plugin;
+
+    @CommandAlias("broadcast|b")
+    @CommandPermission("essentials.broadcast")
+    @Syntax("[-p] <message>")
+    @Description("Broadcast a message")
+    public void onBroadcast(CommandSender sender, String message) {
+        final String[] split = message.split(" ");
+        final boolean asPlayer = (split.length > 1 && split[0].equalsIgnoreCase("-p"));
+
+        Bukkit.broadcastMessage(" ");
+
+        if (asPlayer) {
+            final String trimmed = message.substring(3);
+            Bukkit.broadcastMessage(ChatColor.DARK_AQUA + "[" + ChatColor.DARK_RED + sender.getName() + ChatColor.DARK_AQUA + "] " + ChatColor.AQUA + trimmed);
+        } else {
+            Bukkit.broadcastMessage(ChatColor.DARK_AQUA + "[" + ChatColor.DARK_RED + "Ares" + ChatColor.DARK_AQUA + "] " + ChatColor.AQUA + message);
+        }
+
+        Bukkit.broadcastMessage(" ");
+    }
+}
